54520025b6476cb0377d083dad1a5fe129f754a2
DuyTana273 <duytana273@gmail.com>
2025-02-14 02:52:03 +0700

    Update Theme

--- shoplaptop/src/main/java/com/example/shoplaptop/repository/IProductRepository.java
+++ shoplaptop/src/main/java/com/example/shoplaptop/repository/IProductRepository.java
@@ -9,9 +9,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IProductRepository extends JpaRepository<Product, Integer> {
     Page<Product> findAll(Pageable pageable);
+
+    List<Product> findAll();
 
     Product getById(Integer id);
 
